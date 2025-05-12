5ec8219dd6b4dbe396bc3b900e75bbb29db79aa6
Guy Sananes <guysa2@mta.ac.il>
2025-05-12 14:35:59 +0300

    Update quiz questions to match backend logic, integrate backend request and proposal with styled pie chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { QuizPage } from './Pages/QuizPage/QuizPage';
 import { Navigate, Route, Routes, BrowserRouter } from 'react-router-dom';
