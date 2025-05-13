992f44da9f1e51d33f81d876cb3ab17b0a7be8ba
naorb12 <naorb12@gmail.com>
2025-05-13 12:27:17 +0300

    Fixed markovitz safest portfolio

--- robo-advisor-backend/app/services/investment_advisor.py
+++ robo-advisor-backend/app/services/investment_advisor.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import HTTPException
 
 
 def get_risk_profile(score):
--- robo-advisor-backend/app/services/markovitz_standard.py
+++ robo-advisor-backend/app/services/markovitz_standard.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 import yfinance as yf
 import numpy as np
 import pandas as pd
@@ -10,7 +11,7 @@ end_year = '2025-01-07'
 Num_porSimulation = 200000
 
 def portfolio_optimization(risk_profile: str):
-    frame = yf.download(selected, start=start_year, end=end_year, auto_adjust=False)['Adj Close']
+    frame = yf.download(selected, start=start_year, end=end_year)['Close']
     df = pd.DataFrame(frame).ffill().bfill()
 
     returns_daily = df.pct_change()
@@ -55,18 +56,18 @@ def portfolio_optimization(risk_profile: str):
     min_volatility = df['Volatility'].min()
     max_sharpe = df['Sharpe Ratio'].max()
     max_return = df['Returns'].max()
-    #max_vol = df['Volatility'].max()
+    max_vol = df['Volatility'].max()
 
     sharpe_portfolio = df.loc[df['Sharpe Ratio'] == max_sharpe]
     min_variance_port = df.loc[df['Volatility'] == min_volatility]
     max_returns = df.loc[df['Returns'] == max_return]
-    #max_vols = df.loc[df['Volatility'] == max_vol]
+    max_vols = df.loc[df['Volatility'] == max_vol]
 
     if(risk_profile == "Conservative"):
-        return {"max_volatility": max_vols.to_dict(orient='records')}
+        return {"safest_portfolio": min_variance_port.to_dict(orient='records')}
     elif(risk_profile == "Moderate"):
-        return {"max_sharpe": sharpe_portfolio.to_dict(orient='records')}
+        return {"max_sharpe_portfolio": sharpe_portfolio.to_dict(orient='records')}
     elif(risk_profile == "Aggressive"):
-        return {"max_returns": max_returns.to_dict(orient='records')}
+        return {"max_returns_portfolio": max_returns.to_dict(orient='records')}
     else:
         raise HTTPException(status_code=400, detail="Invalid risk profile. Choose Conservative, Moderate, or Aggressive.")
